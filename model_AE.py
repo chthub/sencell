358d5b1dd01cc0a850f9daeebd23d95d5e3ebae8
Yi Jiang <67355544+jiangyi01@users.noreply.github.com>
2024-09-04 16:27:23 -0400

    Add files via upload

--- model_AE.py
+++ model_AE.py
@@ -4,80 +4,68 @@ from torch.nn import functional as F
 import torch.utils.data as Data
 import numpy as np
 
-# Define the Autoencoder class inheriting from nn.Module
+
 class AE(nn.Module):
     def __init__(self, dim, emb_dim=128):
         super(AE, self).__init__()
-        self.dim = dim  # input dimension
-        # First layer of the encoder part
-        self.fc1 = nn.Linear(dim, 512)  # Maps from input dimension to 512
-        # Second layer of the encoder part, outputs the embedding
-        self.fc2 = nn.Linear(512, emb_dim)  # Reduces dimensionality to emb_dim
-        # First layer of the decoder part
-        self.fc3 = nn.Linear(emb_dim, 512)  # Maps from embedding dimension back to 512
-        # Final layer of the decoder part, outputs the reconstruction
-        self.fc4 = nn.Linear(512, dim)  # Maps back to the original input dimension
+        self.dim = dim
+        self.fc1 = nn.Linear(dim, 512)
+        self.fc2 = nn.Linear(512, emb_dim)
+        self.fc3 = nn.Linear(emb_dim, 512)
+        self.fc4 = nn.Linear(512, dim)
 
     def encode(self, x):
-        # Encoding function applying ReLU activation
         h1 = F.relu(self.fc1(x))
         return F.relu(self.fc2(h1))
 
     def decode(self, z):
-        # Decoding function, reconstructs the input
         h3 = F.relu(self.fc3(z))
         return torch.relu(self.fc4(h3))
 
     def forward(self, x):
-        # Forward pass through the network
-        z = self.encode(x.view(-1, self.dim))  # Encodes the input
-        return self.decode(z), z  # Returns the reconstruction and the embedding
+        z = self.encode(x.view(-1, self.dim))
+        return self.decode(z), z
 
-# Function to perform dimensionality reduction using an autoencoder
+
 def reduction_AE(gene_cell, device):
-    # Convert gene data to tensor and transfer to device (GPU or CPU)
     gene = torch.tensor(gene_cell, dtype=torch.float32).to(device)
-    # Determine batch size based on the size of data
     if gene_cell.shape[0] < 5000:
-        ba = gene_cell.shape[0]  # Use full data if small enough
+        ba = gene_cell.shape[0]
     else:
-        ba = 5000  # Use maximum batch size of 5000
-    # Train the autoencoder for gene data
+        ba = 5000
     gene_embed = train_AE(gene, ba, device)
 
-    # Process for cell data, similar to above but transposed
     if gene_cell.shape[1] < 5000:
         ba = gene_cell.shape[1]
     else:
         ba = 5000
-    cell = torch.tensor(np.transpose(gene_cell), dtype=torch.float32).to(device)
+    cell = torch.tensor(np.transpose(gene_cell),
+                        dtype=torch.float32).to(device)
     cell_embed = train_AE(cell, ba, device)
-
     return gene_embed, cell_embed
 
-# Function to train the autoencoder
+
 def train_AE(feature, ba, device, alpha=0.5, is_init=False):
-    model = AE(dim=feature.shape[1]).to(device)  # Instantiate the AE model
-    model.train()  # Set the model to training mode
+    model = AE(dim=feature.shape[1]).to(device)
+    model.train()
+    
+    loader = Data.DataLoader(feature, ba)
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-    loader = Data.DataLoader(feature, batch_size=ba)  # Create data loader
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)  # Define the optimizer
-
-    loss_func = nn.MSELoss()  # Mean Squared Error loss for reconstruction
-    EPOCH_AE = 2000  # Number of epochs to train
+    loss_func = nn.MSELoss()
+    EPOCH_AE = 2000
     for epoch in range(EPOCH_AE):
-        embeddings = []  # List to store embeddings from each batch
+        embeddings = []
+        # loss_ls=[]
         for _, batch_x in enumerate(loader):
-            decoded, encoded = model(batch_x)  # Perform a forward pass
-            loss = loss_func(batch_x, decoded)  # Compute the loss
+            decoded, encoded = model(batch_x)
+            loss = loss_func(batch_x, decoded)
 
-            optimizer.zero_grad()  # Clear gradients
-            loss.backward()  # Backpropagate the error
-            optimizer.step()  # Update weights
-            embeddings.append(encoded)  # Collect embeddings
-
-        # Output training progress
-        if epoch % 100 == 0:  # Print loss every 100 epochs
-            print('Epoch :', epoch, '|', 'train_loss:%.12f' % loss.data)
-
-    return torch.cat(embeddings)  # Concatenate all embeddings into a single tensor
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            embeddings.append(encoded)
+        #     loss_ls.append(loss.item())
+        # scheduler.step(np.mean(loss_ls))
+    print('Epoch :', epoch, '|', 'train_loss:%.12f' % loss.data)
+    return torch.cat(embeddings)
--- model_Sencell.py
+++ model_Sencell.py
@@ -5,23 +5,16 @@ from collections import defaultdict
 import numpy as np
 import torch
 import torch.utils.data as Data
-from linetimer import CodeTimer  # Utility to time code execution
+from linetimer import CodeTimer
 from torch import nn, optim
 from torch.nn import Dropout, Linear, ReLU
 from torch.nn import functional as F
-from tqdm import tqdm  # For displaying progress bars
+from tqdm import tqdm
 
-# Function to map clusters to their respective cells
+
 def get_cluster_cell_dict(sencell_dict, nonsencell_dict):
-    """
-    Creates dictionaries to map cluster IDs to lists of cell indices for senescent and non-senescent cells.
-    Arguments:
-    - sencell_dict: Dictionary of senescent cells.
-    - nonsencell_dict: Dictionary of non-senescent cells.
-    
-    Returns:
-    - tuple of two defaultdicts: (cluster_sencell, cluster_nonsencell)
-    """
+    # 计算cluster所包含的cell的映射表
+    # {cluster: [cell_indexs]}
     cluster_sencell = defaultdict(list)
     cluster_nonsencell = defaultdict(list)
 
@@ -33,159 +26,302 @@ def get_cluster_cell_dict(sencell_dict, nonsencell_dict):
 
     return cluster_sencell, cluster_nonsencell
 
-# Function to compute prototype embeddings for each cluster
+
 def getPrototypeEmb(sencell_dict, cluster_sencell, emb_pos=2):
-    """
-    Calculates the prototype embeddings for senescent cells by averaging embeddings within each cluster.
-    Arguments:
-    - sencell_dict: Dictionary of senescent cells.
-    - cluster_sencell: Dictionary mapping clusters to senescent cell indices.
-    - emb_pos: Position in the dictionary where embeddings are stored.
-    
-    Returns:
-    - prototype_emb: Dictionary of prototype embeddings for each cluster.
-    """
+    # 计算prototype的embedding，只计算老化细胞的prototype
     prototype_emb = {}
     for key, value in cluster_sencell.items():
-        embs = [sencell_dict[i][emb_pos].view(1, -1) for i in value]
-        prototype_emb[key] = torch.mean(torch.cat(embs), 0)
+        embs = []
+        for i in value:
+            embs.append(sencell_dict[i][emb_pos].view(1, -1))
+        embs = torch.cat(embs)
+        prototype_emb[key] = torch.mean(embs, 0)
 
     return prototype_emb
 
-# Senescent cell model class
-class Sencell(nn.Module):
-    def __init__(self, dim=160):
+
+class Sencell(torch.nn.Module):
+    def __init__(self, dim=128):
         super().__init__()
-        # Neural network layers for transforming embeddings
-        self.linear1 = Linear(dim, 256)
-        self.linear2 = Linear(256, 256)
-        self.linear21 = Linear(256, 256)
-        self.linear22 = Linear(256, 256)
-        self.linear3 = Linear(256, 256)
-        self.linear4 = Linear(256, dim)
+        self.hidden_size=128
+        self.linear1 = Linear(dim, self.hidden_size)
+        self.linear2 = Linear(self.hidden_size, self.hidden_size)
+        self.linear21 = Linear(self.hidden_size, self.hidden_size)
+        self.linear22 = Linear(self.hidden_size, self.hidden_size)
+        self.linear3 = Linear(self.hidden_size, self.hidden_size)
+        self.linear4 = Linear(self.hidden_size, dim)
+        # encoder_layer = torch.nn.TransformerEncoderLayer(d_model=128, nhead=8)
+        # self.transformer_encoder = torch.nn.TransformerEncoder(encoder_layer,
+        #                                                        num_layers=2)
+        self.act = torch.nn.CELU()
+        self.layer_norm = nn.LayerNorm(self.hidden_size)
+        self.levels = torch.nn.Parameter(torch.tensor([0, 0, 4., 4]),
+                                         requires_grad=True)
+        # self.levels = torch.tensor([-3.0, -1, 1, 3.0])
+        self.device = None
 
-        self.act = nn.CELU()  # Activation function
-        self.layer_norm = nn.LayerNorm(256)  # Layer normalization
-        self.levels = nn.Parameter(torch.tensor([0, 0, 4., 4]), requires_grad=True)  # Levels for adjusting model learning
-
-    # Method to concatenate embeddings from senescent and non-senescent cells
     def catEmbeddings(self, sencell_dict, nonsencell_dict):
-        embeddings = [v[0].view(1, -1) for v in sencell_dict.values()] + [v[0].view(1, -1) for v in nonsencell_dict.values()]
+        embeddings = []
+        for key, value in sencell_dict.items():
+            embeddings.append(value[0].view(1, -1))
+        for key, value in nonsencell_dict.items():
+            embeddings.append(value[0].view(1, -1))
         return torch.cat(embeddings)
 
-    # Method to update dictionaries with new embeddings
     def updateDict(self, x, sencell_dict, nonsencell_dict):
         count = 0
-        for key in sencell_dict:
+        for key, value in sencell_dict.items():
             sencell_dict[key][2] = x[count]
             count += 1
-        for key in nonsencell_dict:
+        for key, value in nonsencell_dict.items():
             nonsencell_dict[key][2] = x[count]
             count += 1
+
         return sencell_dict, nonsencell_dict
 
-    # Forward pass of the model
     def forward(self, sencell_dict, nonsencell_dict, device):
         x = self.catEmbeddings(sencell_dict, nonsencell_dict).to(device)
         self.device = device
 
-        # Process embeddings through linear layers and activations
-        x = self.act(self.linear1(x))
-        x = x + self.act(self.linear2(x))
-        x = self.layer_norm(x)
-        x = x + self.act(self.linear22(self.act(self.linear21(x))))
-        x = self.layer_norm(x)
+        # model1
+        # x = self.linear2(self.act(self.linear1(x)))
+        # x = self.linear4(self.act(self.linear3(x)))
+        
+        # model 2
+        x=self.act(self.linear1(x))
+        x=x+self.act(self.linear2(x))
+        x=self.layer_norm(x)
+        x=x+self.act(self.linear22(self.act(self.linear21(x))))
+        x=self.layer_norm(x)
         x = self.linear4(self.act(self.linear3(x)))
 
         result = self.updateDict(x, sencell_dict, nonsencell_dict)
         return result
-
-    # Additional methods for distance calculations and loss functions omitted for brevity
-
-# Function to process dictionary elements
-def process_dict(cell_dict, dgl_graph, args):
-    """
-    Updates the cell dictionary with position encodings from a graph if available.
-    Arguments:
-    - cell_dict: Dictionary of cells, each key containing various cell properties.
-    - dgl_graph: Graph structure that possibly contains additional node features.
-    - args: Command line arguments or other configurations.
     
-    Returns:
-    - cell_dict: Updated cell dictionary with concatenated position encodings if dgl_graph is provided.
-    """
+    def get_embeddings(self,embs,device):
+        x = embs.to(device)
+        self.device = device
+
+        # model1
+        # x = self.linear2(self.act(self.linear1(x)))
+        # x = self.linear4(self.act(self.linear3(x)))
+        
+        # model 2
+        x=self.act(self.linear1(x))
+        x=x+self.act(self.linear2(x))
+        x=self.layer_norm(x)
+        x=x+self.act(self.linear22(self.act(self.linear21(x))))
+        x=self.layer_norm(x)
+        x = self.linear4(self.act(self.linear3(x)))
+        
+        return x
+    
+
+    def prototypeLoss(self, distances):
+        results = 0
+        for cluster_distance in distances:
+            results += cluster_distance[0].square().sum().sqrt()
+
+        return results/len(distances)
+
+    def eucliDistance(self, v1, v2):
+        # 计算欧氏距离
+        return F.pairwise_distance(v1.view(1, -1), v2.view(1, -1), p=2)
+
+    def get_d1(self, sencell_dict, cluster_sencell, prototype_emb, emb_pos=2):
+        d1 = []
+        for cluster, prototype in prototype_emb.items():
+            # 对于只有一个老化细胞的情况，distance就是0，但是显示1.1314e-05
+            distance_ls = []
+            for cell_index in cluster_sencell[cluster]:
+                cell_emb = sencell_dict[cell_index][emb_pos]
+                distance = self.eucliDistance(cell_emb, prototype)
+                distance_ls.append(distance)
+            d1.append(distance_ls)
+
+        return d1
+
+    def get_d2(self, sencell_dict, cluster_sencell, prototype_emb, emb_pos=2):
+        # d2表示不同簇的老化细胞之间的距离
+        d2 = []
+        for cluster, prototype in prototype_emb.items():
+            distance_ls = []
+            for another_cluster, cell_indexs in cluster_sencell.items():
+                if another_cluster != cluster:
+                    for cell_index in cell_indexs:
+                        cell_emb = sencell_dict[cell_index][emb_pos]
+                        distance = self.eucliDistance(cell_emb, prototype)
+                        distance_ls.append(distance)
+            d2.append(distance_ls)
+        return d2
+
+    def get_d3(self, nonsencell_dict, cluster_nonsencell, prototype_emb, emb_pos=2):
+        # d3表示同一细胞类型内部老化细胞和非老化细胞之间的距离
+        d3 = []
+        for cluster, prototype in prototype_emb.items():
+            distance_ls = []
+            if cluster not in cluster_nonsencell:
+                # 这是我们要避免出现的情况
+                # 这是一种特殊情况要单独处理，如果没有非老化细胞，就没有必要优化d3了
+                # 这里有两种解决方法，一种是直接d3.append([])，但这样需要后续的逻辑考虑到这种情况
+                # 另一种方法是在前面采样的时候就避免出现某一簇没有非老化细胞的情况
+                # 这个问题先一放
+                print("这一簇没有非老化细胞：", cluster)
+                distance_ls.append(torch.tensor([0.75]).to(self.device))
+            else:
+                for nonsencell_index in cluster_nonsencell[cluster]:
+                    nonsencell_emb = nonsencell_dict[nonsencell_index][emb_pos]
+                    distance = self.eucliDistance(nonsencell_emb, prototype)
+                    distance_ls.append(distance)
+            d3.append(distance_ls)
+        return d3
+
+    def get_d4(self, nonsencell_dict, cluster_nonsencell, prototype_emb, emb_pos=2):
+        # d4表示不同细胞类型之间，老化和非老化之间的距离
+        d4 = []
+        for cluster, prototype in prototype_emb.items():
+            distance_ls = []
+            for another_cluster, nonsencell_indexs in cluster_nonsencell.items():
+                if another_cluster != cluster:
+                    for nonsencell_index in nonsencell_indexs:
+                        nonsencell_emb = nonsencell_dict[nonsencell_index][emb_pos]
+                        distance = self.eucliDistance(
+                            nonsencell_emb, prototype)
+                        distance_ls.append(distance)
+            d4.append(distance_ls)
+        return d4
+
+    def caculateDistance(self, sencell_dict, nonsencell_dict,
+                         cluster_sencell, cluster_nonsencell,
+                         prototype_emb, emb_pos=2):
+        # d1: 老化细胞簇内prototype和每个cell之间的距离
+        # 对于每个拥有老化细胞的cluster，都会有d1
+        d1 = self.get_d1(sencell_dict, cluster_sencell, prototype_emb, emb_pos)
+        # d2表示不同簇的老化细胞之间的距离
+        d2 = self.get_d2(sencell_dict, cluster_sencell, prototype_emb, emb_pos)
+        # d3表示同一细胞类型内部老化细胞和非老化细胞之间的距离
+        d3 = self.get_d3(nonsencell_dict, cluster_nonsencell,
+                         prototype_emb, emb_pos)
+        # d4表示不同细胞类型之间，老化和非老化之间的距离
+        # d4 = self.get_d4(nonsencell_dict, cluster_nonsencell,
+        #                  prototype_emb, emb_pos)
+        return d1, d2, d3
+
+    def getMultiLevelDistanceLoss(self, distances):
+        d1, d2, d3 = distances
+        result = 0
+
+        def distanceDiff(cluster_d, level):
+            # cluster_d是同一cluster的同一类型的distance的列表
+            count = 0
+            result = 0
+            for d in cluster_d:
+                result += (d-level).abs()
+                count += 1
+            if count==0:
+                return 0
+            return result/count
+
+        for cluster_d_1, cluster_d_2, cluster_d_3 in zip(d1, d2, d3):
+            result += distanceDiff(cluster_d_1, self.levels[0])
+            result += distanceDiff(cluster_d_2, self.levels[1])
+            result += distanceDiff(cluster_d_3, self.levels[2])
+            # result += distanceDiff(cluster_d_4, self.levels[3])
+
+        return result
+
+    def loss(self, sencell_dict, nonsencell_dict):
+        # step 1: 计算cluster和cell的映射表
+        cluster_sencell, cluster_nonsencell = get_cluster_cell_dict(
+            sencell_dict, nonsencell_dict)
+        # step 2: 计算老化细胞簇的prototype embedding
+        prototype_emb = getPrototypeEmb(sencell_dict, cluster_sencell)
+        # step 3: 计算不同的distance
+        distances = self.caculateDistance(sencell_dict, nonsencell_dict,
+                                          cluster_sencell, cluster_nonsencell,
+                                          prototype_emb)
+        # step 4: 计算multi-level distance
+        loss = self.getMultiLevelDistanceLoss(distances)
+
+        return loss
+
+
+def process_dict(cell_dict,dgl_graph,args):
     if dgl_graph is None:
-        # If there is no graph data available, return the dictionary unchanged
-        return cell_dict
-    else:
-        # Update each cell's data with positional encodings from the graph
         for key in cell_dict:
-            cell_index = cell_dict[key][-1] - args.gene_num
-            pos_enc = dgl_graph.nodes[cell_index].data['pos_enc'].reshape(-1,)
-            cell_dict[key][0] = torch.cat([cell_dict[key][0], pos_enc])
-        return cell_dict
+            cell_index=cell_dict[key][-1]-args.gene_num
+    else:
+        for key in cell_dict:
+            cell_index=cell_dict[key][-1]-args.gene_num
+            cell_dict[key][0]=torch.cat([cell_dict[key][0], 
+                                        dgl_graph.nodes[cell_index].data['pos_enc'].reshape(-1,)
+                                        ])
+    return cell_dict
 
-def cell_optim(cellmodel, optimizer, sencell_dict, nonsencell_dict, dgl_graph, args, train=False, wandb=None):
-    """
-    Trains or loads a cell model, optimizing it based on the specified dictionaries of senescent and non-senescent cells.
-    Arguments:
-    - cellmodel: The cell model to be trained or evaluated.
-    - optimizer: Optimizer used for training.
-    - sencell_dict: Dictionary containing data of senescent cells.
-    - nonsencell_dict: Dictionary containing data of non-senescent cells.
-    - dgl_graph: Graph containing additional node features.
-    - args: Additional arguments or configurations.
-    - train: Flag to indicate if the model should be trained.
-    - wandb: Optional Weights & Biases logging.
 
-    Returns:
-    - cellmodel: Trained or loaded cell model.
-    - sencell_dict: Updated dictionary of senescent cells.
-    - nonsencell_dict: Updated dictionary of non-senescent cells.
-    """
+def cell_optim(cellmodel, optimizer, sencell_dict, nonsencell_dict,dgl_graph, args, train=False,wandb=None):
+    # optimizer = torch.optim.RMSprop(cellmodel.parameters(), lr=0.1, alpha=0.5,
+    #                                 weight_decay=1e-4)
     if train:
         cellmodel.train()
-        # Process cell dictionaries with potentially new data from a graph
-        sencell_dict = process_dict(sencell_dict, dgl_graph, args)
-        nonsencell_dict = process_dict(nonsencell_dict, dgl_graph, args)
-
+        sencell_dict=process_dict(sencell_dict,dgl_graph,args)
+        nonsencell_dict=process_dict(nonsencell_dict,dgl_graph,args)
+        
         for epoch in range(args.cell_optim_epoch):
             optimizer.zero_grad()
-            # Update dictionaries with the latest model output
-            sencell_dict, nonsencell_dict = cellmodel(sencell_dict, nonsencell_dict, args.device)
-            loss = cellmodel.loss(sencell_dict, nonsencell_dict)  # Compute loss
+            sencell_dict, nonsencell_dict = cellmodel(
+                sencell_dict, nonsencell_dict, args.device)
+            loss = cellmodel.loss(sencell_dict, nonsencell_dict)
             print(loss.item())
             if wandb is not None:
-                wandb.log({"loss": loss})  # Log loss if Weights & Biases is used
+                wandb.log({"loss":loss})
             loss.backward()
             optimizer.step()
 
-        # Save the trained model
-        torch.save(cellmodel, os.path.join(args.output_dir, f'{args.exp_name}_cellmodel.pt'))
+        torch.save(cellmodel, os.path.join(
+            args.output_dir, f'{args.exp_name}_cellmodel.pt'))
     else:
-        # Load the model if not training
-        cellmodel = torch.load(os.path.join(args.output_dir, f'{args.exp_name}_cellmodel.pt'))
-        sencell_dict, nonsencell_dict = cellmodel(sencell_dict, nonsencell_dict, args.device)
+        cellmodel = torch.load(os.path.join(
+            args.output_dir, f'{args.exp_name}_cellmodel.pt'))
+        sencell_dict, nonsencell_dict = cellmodel(
+            sencell_dict, nonsencell_dict, args.device)
 
     return cellmodel, sencell_dict, nonsencell_dict
 
+
+def old_cell_optim(sencell_dict, nonsencell_dict, device, retrain=False):
+    cellmodel = Sencell().to(device)
+    optimizer = torch.optim.Adam(cellmodel.parameters(), lr=0.001,
+                                 weight_decay=1e-3)
+    # optimizer = torch.optim.RMSprop(cellmodel.parameters(), lr=0.1, alpha=0.5,
+    #                                 weight_decay=1e-4)
+    if retrain:
+        cellmodel.train()
+        for epoch in range(20):
+            optimizer.zero_grad()
+            # print(cellmodel.levels)
+            sencell_dict, nonsencell_dict = cellmodel(
+                sencell_dict, nonsencell_dict, device)
+            loss = cellmodel.loss(sencell_dict, nonsencell_dict)
+            print(loss.item())
+            loss.backward()
+            optimizer.step()
+
+        torch.save(cellmodel, './cellmodel1.pt')
+    else:
+        cellmodel = torch.load('./cellmodel1.pt')
+        cellmodel.eval()
+        sencell_dict, nonsencell_dict = cellmodel(
+            sencell_dict, nonsencell_dict, device)
+    return sencell_dict, nonsencell_dict
+
+
 def update_cell_embeddings(sampled_graph, sencell_dict, nonsencell_dict):
-    """
-    Updates the embeddings in a graph based on the provided cell dictionaries.
-    Arguments:
-    - sampled_graph: Graph whose embeddings are to be updated.
-    - sencell_dict: Dictionary of senescent cells with their embeddings.
-    - nonsencell_dict: Dictionary of non-senescent cells with their embeddings.
-    
-    Returns:
-    - sampled_graph: Graph with updated embeddings.
-    """
     feature = sampled_graph.x
     for key, value in sencell_dict.items():
-        feature[key] = value[2].detach()  # Update senescent cell embeddings
+        feature[key] = value[2].detach()
     for key, value in nonsencell_dict.items():
-        feature[key] = value[2].detach()  # Update non-senescent cell embeddings
+        feature[key] = value[2].detach()
 
     sampled_graph.x = feature
     return sampled_graph
-
